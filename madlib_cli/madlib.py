cae66d8edf88e7f87874fd57440f933ee7491b38
Marleyman876 <grant_garfield@yahoo.com>
2021-06-13 19:32:31 -0700

    got it working

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -2,28 +2,27 @@ import re
 
 
 def read_template(read_file):
-    with open(read_file) as file:
-        return file.read().strip()
+  with open(read_file) as file:
+    try:
+      return file.read().strip()
+    except FileNotFoundError:
+      return 'File not Found'
 
-
-
-#Got help with this from Kassie 
+# Got help with this from Kassie 
 def parse_template(string):
   
-  incoming_string = string
+  new_string = string
   
-  parts = tuple(re.findall(r"\{(.*?\}", incoming_string))
-  
-  string = re.sub(r"\{(.*?\}", "{}", incoming_string)
+  parts = tuple(re.findall(r"\{(.*?)\}", new_string))
+  string = re.sub(r"\{(.*?)\}", "{}", new_string)
   
   print(string, parts)
   return(string, parts)
-   
-print(parse_template(string))
     
 
-def test_merge():
-  pass
+def merge(str, tuple):
+  return str.format(*tuple)
+
 
 if __name__ == "__main__":
     path = 'assets/dark_and_stromy_night_template.txt'
